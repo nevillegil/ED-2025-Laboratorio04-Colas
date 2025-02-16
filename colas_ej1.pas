cb0122b2a5dce59caecb31506d4480d485b3ede3
Sergio Cavero <sergio.cavero@urjc.es>
2025-02-16 10:48:52 +0100

    Add queue implementation and example usage in Pascal

---
+++ colas_ej1.pas
@@ -0,0 +1,45 @@
+program colas_ej1.pas;
+
+uses uColaInteger;
+
+var
+    cola: tCola;
+
+begin
+    // Ejercicio 1.2: Crear una cola, añadir elementos y mostrar la cola
+    // 1.2.1 Crear una cola
+
+    // 1.2.2 Añadir los elementos 1, 2, 3, 4 y 5 a la cola
+
+    // 1.2.3 Mostrar los elementos de la cola
+
+    // Ejercicio 1.3: Trabajar con el primer y último elemento de la cola
+    // 1.3.1 Mostrar el primer elemento de la cola
+
+    // 1.3.2 Mostrar el último elemento de la cola
+
+    // 1.3.3 Mostrar la diferencia entre el primer y el último elemento de la cola (primero - último)
+
+    // Ejercicio 1.4: Eliminar elementos de la cola
+    // 1.4.1. Muestra el número de elementos de la cola.
+
+    // 1.4.2 Eliminar el primer elemento de la cola
+
+    // 1.4.3 Mostrar los elementos de la cola
+
+    // 1.4.4. Muestra el número de elementos de la cola.
+
+    // 1.4.5 Eliminar todos los elementos de la cola
+
+    // 1.4.6 Mostrar los elementos de la cola
+
+    // 1.4.7. Muestra el número de elementos de la cola.
+
+    // Ejercicio 1.5: Consultar si la cola está vacía
+    // 1.5.1 Consultar si la cola está vacía
+
+    // 1.5.2 Añadir el elemento 1 a la cola
+
+    // 1.5.3 Consultar si la cola está vacía
+
+end.
